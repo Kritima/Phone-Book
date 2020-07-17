ec7617cd9e84a795d5bd6a4b063f0c2f29dd2e8a
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-07-16 21:18:53 -0400

    Variables added and initialised

--- app/src/main/java/com/lambton/phone_book/DetailsActivity.java
+++ app/src/main/java/com/lambton/phone_book/DetailsActivity.java
@@ -2,13 +2,49 @@ package com.lambton.phone_book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    Button add;
+    TextInputEditText firstName;
+    TextInputEditText lastName;
+    TextInputEditText address;
+    TextInputEditText city;
+    TextInputEditText province;
+    TextInputEditText postalCode;
+    TextInputEditText email;
+    TextInputEditText phone;
+    TextInputEditText area;
+    myDbAdapter helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details2);
+
+        firstName = (TextInputEditText) findViewById(R.id.firstNameTextInputEditText);
+        lastName = (TextInputEditText) findViewById(R.id.lastNameTextInputEditText);
+        address = (TextInputEditText) findViewById(R.id.AddressTextInputEditText);
+        city = (TextInputEditText) findViewById(R.id.CityTextInputEditText);
+        province = (TextInputEditText) findViewById(R.id.ProvinceTextInputEditText);
+        postalCode = (TextInputEditText) findViewById(R.id.PostalCodeTextInputEditText);
+        email = (TextInputEditText) findViewById(R.id.EmailTextInputEditText);
+        phone = (TextInputEditText) findViewById(R.id.PhoneTextInputEditText);
+        area = (TextInputEditText) findViewById(R.id.AreaTextInputEditText);
+
+        helper = new myDbAdapter(this);
+
+
+
     }
 }
